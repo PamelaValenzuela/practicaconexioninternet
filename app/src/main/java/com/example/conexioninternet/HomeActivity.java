76925ddd85e3c1564acef50c03b244466c2e71e2
pamelaValenzuela <pamela.valenzuela@usach.cl>
2021-02-08 14:25:11 -0300

    First commit" funciona sin imagen"

---
+++ app/src/main/java/com/example/conexioninternet/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.conexioninternet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
