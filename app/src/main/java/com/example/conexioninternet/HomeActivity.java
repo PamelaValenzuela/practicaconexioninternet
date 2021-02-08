cedbe4792cbea5af82ae5ff146371805a80ec1a7
pamelaValenzuela <pamela.valenzuela@usach.cl>
2021-02-08 16:12:13 -0300

    3 commit"compila y además verifica si existe conexión"

--- app/src/main/java/com/example/conexioninternet/HomeActivity.java
+++ app/src/main/java/com/example/conexioninternet/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.conexioninternet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,11 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //no estoy segura si se debe llamar al metodo o solo basta con el onclick en el layout
+    }
+    public void openActivityCheckConnection(View v){
+        Intent intent = new Intent(this, HomeDetailActivity.class);
+        startActivity(intent);
     }
 }
